e506751195016389df5a608aa6b268c90ca557ea
Ann Wanjeri <wannjeri2@gmail.com>
2022-02-07 13:37:44 +0300

    ts-test code for js

--- script.js
+++ script.js
@@ -51,7 +51,12 @@ const quiz= document.getElementById('quiz')
 const answerElis= document.querySelectorAll('.answer')
 const questionEl= document.getElementById('question')
 
+const a_text=document.getElementById('a_text')
+const b_text=document.getElementById('b_text')
+const c_text=document.getElementById('c_text')
+const d_text=document.getElementById('d_text')
 
+const submitButn=document.getElementById('submit')
 
 
 
