3875810f20f03067de662fa8607866f3c5978945
Ann Wanjeri <wannjeri2@gmail.com>
2022-02-07 13:33:28 +0300

    ts-js test code

--- script.js
+++ script.js
@@ -1,4 +1,56 @@
 
+const quizData = [
+    {
+        qustion:"Which language runs in a web browser?",
+        a:"Java",
+        b:"Python",
+        c:"C",
+        d:"Javascript",
+        correct:"d",
+    },
+
+    {
+        qustion:"What year was Javascript launched?",
+        a:"1996",
+        b:"1995",
+        c:"1994",
+        d:"none of the above",
+        correct:"b",
+    },
+    
+    {
+        qustion:"Which of the folowing is not a data type?",
+        a:"Booleans",
+        b:"Numbers",
+        c:"Variables",
+        d:"Strigs",
+        correct:"c",
+    },
+
+    {
+        qustion:"Identify the logical operator in the list below?",
+        a:"===",
+        b:"<=",
+        c:"&&",
+        d:"+=",
+        correct:"c",
+    },
+
+    {
+        qustion:"Which of the following is not a promitive data type",
+        a:"String",
+        b:"Array",
+        c:"Number",
+        d:"Boolean",
+        correct:"b",
+    },
+
+];
+
+const quiz= document.getElementById('quiz')
+const answerElis= document.querySelectorAll('.answer')
+const questionEl= document.getElementById('question')
+
 
 
 
